2bc36b8e692c13e27bfb58a14d10b3570b127e47
Васина Вроника <wise.init@gmail.com>
2021-02-11 20:20:54 +0300

    form misprint

--- cm/main.py
+++ cm/main.py
@@ -1,15 +1,25 @@
 from flask import Flask, request
-from pprint import pprint
+# from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-	data = pprint.pformat(request.form)
- 	return '''<html><h1>Form</h1>
- 		<from>
- 		<input name='veryvar' />
- 		<button />
- 		</form>
- 		</html>''' + data
+	data = request.form
+	data = repr(data)
+
+	# data = 'request.data'
+
+	# if request.method == "POST":
+	# 	data = request.form.get("veryvar")
+	# else:
+	# 	data = request.form["veryvar"]
+
+	return '''<html><h1>Form</h1>
+		<form action="" method="post">
+		<input name='veryvar' />
+		<input name='veryvar111' />
+		<button type="submit" class="btn btn-primary">Send</button>
+		</form>
+		''' + str(data) + '</html>'
